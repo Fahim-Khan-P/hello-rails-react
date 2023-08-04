104f92cab0751ba3d0089a5a0572b4a1ab1e149e
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-08-05 01:00:48 +0430

    Update routes

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::GreetingsController < ApplicationController
+  def random
+    @greeting = Message.order("RANDOM()").first
+    render json: { greeting: @greeting.content }
+  end
 end
